6f1d638e7bf784fe72f240fcf1c19cc3c00dc31c
Sarthak Gupta <118931056+codinglover0@users.noreply.github.com>
2023-01-05 00:12:36 +0530

    Add files via upload

---
+++ FCFS.py
@@ -0,0 +1,32 @@
+                    #FCFS DISK SCHEDULING ALGORITHM
+
+#request queue is the sequence of desired track numbers to reaad/write data.
+request_queue=[98,183,37,122,14,124,65,67]
+l=[]
+
+#initialise seek time to zero
+seek_time=0
+total_head_movements=0
+final_position=0
+
+#current posititon is the track number where arm head is present initially.
+current_position=53
+
+while(len(request_queue)>0):
+    seek_time=abs(current_position-request_queue[0])
+    l.append(seek_time)
+    current_position=request_queue[0]
+    request_queue.remove(request_queue[0])
+    seek_time=0
+
+#printing total no. of head movements.        
+for i in range(len(l)):        
+    total_head_movements+=l[i]
+print('Total head movements are:',total_head_movements)    
+
+
+
+
+
+
+
